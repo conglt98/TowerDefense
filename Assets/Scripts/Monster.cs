7e695d8ab988c5b8678c605cf58cdb4d9bdbb355
nqvuong1998 <nqvuong1998@gmail.com>
2019-11-09 01:11:57 +0700

    7.2 - Spawn effect

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -7,5 +7,23 @@ public class Monster : MonoBehaviour
     public void Spawn()
     {
         transform.position = LevelManager.Instance.BluePortal.transform.position;
+
+        StartCoroutine(Scale(new Vector3(0.01f, 0.01f), new Vector3(0.2f, 0.2f)));
+    }
+
+    public IEnumerator Scale(Vector3 from, Vector3 to)
+    {
+        float progress = 0;
+
+        while(progress <= 1)
+        {
+            transform.localScale = Vector3.Lerp(from, to, progress);
+
+            progress += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localScale = to;
     }
 }
