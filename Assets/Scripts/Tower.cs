4d06624aa7795af5861d280e2e29d2d2aac5ad66
nqvuong1998 <nqvuong1998@gmail.com>
2019-11-10 22:34:23 +0700

    9.2 - Projectiles

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    [SerializeField]
+    private string projectileType;
+
     private SpriteRenderer mySpriteRenderer;
 
     private Monster target;
 
     private Queue<Monster> monsters = new Queue<Monster>();
+
+    private bool canAttack = true;
+
+    private float attackTimer;
+
+    [SerializeField]
+    private float attackCooldown;
 
     // Start is called before the first frame update
     void Start()
@@ -30,10 +40,37 @@ public class Tower : MonoBehaviour
 
     private void Attack()
     {
+        if (!canAttack)
+        {
+            attackTimer += Time.deltaTime;
+            if (attackTimer >= attackCooldown)
+            {
+                canAttack = true;
+                attackTimer = 0;
+            }
+        }
+
         if(target == null && monsters.Count > 0)
         {
             target = monsters.Dequeue();
+
         }
+        if(target != null && target.IsActive)
+        {
+            if (canAttack)
+            {
+                Shoot();
+                canAttack = false;
+            }
+            
+        }
+    }
+
+    private void Shoot()
+    {
+        Projectile projectile = GameManager.Instance.Pool.GetObject(projectileType).GetComponent<Projectile>();
+
+        projectile.transform.position = transform.position;
     }
 
     public void OnTriggerEnter2D(Collider2D other)
