1c678a818febebc87fcb5055616a881b9e015be1
nqvuong1998 <nqvuong1998@gmail.com>
2019-11-10 21:48:25 +0700

    9.1 - Tower targets

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,6 +6,10 @@ public class Tower : MonoBehaviour
 {
     private SpriteRenderer mySpriteRenderer;
 
+    private Monster target;
+
+    private Queue<Monster> monsters = new Queue<Monster>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +19,36 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Attack();
+        Debug.Log(target);
     }
 
     public void Select()
     {
         mySpriteRenderer.enabled = !mySpriteRenderer.enabled;
+    }
+
+    private void Attack()
+    {
+        if(target == null && monsters.Count > 0)
+        {
+            target = monsters.Dequeue();
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Monster")
+        {
+            monsters.Enqueue(other.GetComponent<Monster>());
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Monster")
+        {
+            target = null;
+        }
     }
 }
