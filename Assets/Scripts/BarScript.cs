19caa68358a71da97fc1b4e92437d9cb313fd1e5
1612842 <1612842@student.hcmus.edu.vn>
2019-11-13 20:51:51 +0700

    10.0 - Health bars

---
+++ Assets/Scripts/BarScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour
+{
+    [SerializeField]
+    private Image content;
+
+    [SerializeField]
+    private Text valueText;
+
+    [SerializeField]
+    private float lerpSpeed;
+
+    private float fillAmount;
+
+    [SerializeField]
+    private bool lerpColors;
+
+    [SerializeField]
+    private Color fullColor;
+
+    [SerializeField]
+    private Color lowColor;
+
+    public float MaxValue { get; set; }
+
+    private float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Stat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+class Stat
+{
+    [SerializeField]
+    private BarScript bar;
+
+    [SerializeField]
+    private float maxVal;
+
+    [SerializeField]
+    private float currentVal;
+
+    public float CurrentValue { get => currentVal; set => currentVal = value; }
+    public float MaxVal { get => maxVal; set => maxVal = value; }
+}
